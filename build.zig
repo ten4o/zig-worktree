b6a1d9f0d44f5e020c5070ff3f6e03201732b5b5
Tencho Dimitrov <tencho@tencho.tk>
2023-03-31 11:55:35 +0300

    fix build broken by new zig version

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -10,13 +10,16 @@ pub fn build(b: *std.build.Builder) void {
 
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable("zig-worktree", "src/main.zig");
+    const exe = b.addExecutable(.{
+        .name = "zig-worktree",
+        .root_source_file = std.build.FileSource.relative("src/main.zig"),
+        .target = target,
+        .optimize = optimize
+    });
     addPaths(exe);
     exe.linkSystemLibrary("git2");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
     exe.linkLibC();
     exe.install();
 
@@ -29,22 +32,26 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
+    const exe_tests = b.addTest(.{
+        .name = "zig-worktree",
+        .root_source_file = std.build.FileSource.relative("src/main.zig"),
+        .target = target,
+        .optimize = optimize
+    });
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
 }
 
 // add `include` and `lib` paths
-fn addPaths(exe: *std.build.LibExeObjStep) void {
+fn addPaths(exe: *std.build.CompileStep) void {
     switch (builtin.target.os.tag) {
         .windows => {
             exe.addIncludePath("./deps/include");
             exe.addLibraryPath(thisDir() ++ "/deps/lib");
         },
         .freebsd, .openbsd, .netbsd, .linux => {
+            // use system paths
         },
         .macos => {
             exe.addIncludePath("/opt/homebrew/include");

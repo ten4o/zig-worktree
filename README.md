226f6febc0d79ad7a5801c5692cd331f025b4def
Tencho Dimitrov <tencho@tencho.tk>
2023-08-04 15:33:29 +0300

    fix to build with zig v0.11.0

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ This is a toy project that I play with. Its main purpose is to help me learn [Zi
 
 It's been tried and somewhat works in Linux, MacOS and Windows 10.
 
-To build it you need [libgit2](https://github.com/libgit2/libgit2).
- - on Linux - use you package manager
- - on MacOS - `brew install libgit2`
- - on Windows - I build libgit2 from source and put header files in `./deps/include/` and lib file in `./deps/lib/`
+To build it you need:
+ - zig version [0.11.0](https://ziglang.org/download) or newer
+ - [libgit2](https://github.com/libgit2/libgit2).
+ 	- on Linux - use you package manager
+    - on MacOS - `brew install libgit2`
+    - on Windows - I build libgit2 from source and put header files in `./deps/include/` and lib file in `./deps/lib/`
 
 Key bindings are hard-coded in the [source](src/key_bind.zig):
  - `k` - up
--- build.zig
+++ build.zig
@@ -2,6 +2,14 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 pub fn build(b: *std.Build) void {
+    comptime {
+        const current_zig = builtin.zig_version;
+        const min_zig = std.SemanticVersion.parse("0.11.0") catch unreachable; // Merge pull request #16446 from MasterQ32/buildsystem_rename_orgy
+        if (current_zig.order(min_zig) == .lt) {
+            @compileError(std.fmt.comptimePrint("Your Zig version v{} does not meet the minimum build requirement of v{}", .{ current_zig, min_zig }));
+        }
+    }
+
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -12,26 +20,14 @@ pub fn build(b: *std.Build) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "zig-worktree",
-        .root_source_file = std.build.FileSource.relative("src/main.zig"),
-        .target = target,
-        .optimize = optimize
-    });
+    const exe = b.addExecutable(.{ .name = "zig-worktree", .root_source_file = std.build.FileSource.relative("src/main.zig"), .target = target, .optimize = optimize });
     addPaths(exe);
     exe.linkSystemLibrary("git2");
     exe.linkLibC();
     //exe.install();
     b.installArtifact(exe);
 
-
-
-    const exe_tests = b.addTest(.{
-        .name = "zig-worktree",
-        .root_source_file = std.build.FileSource.relative("src/main.zig"),
-        .target = target,
-        .optimize = optimize
-    });
+    const exe_tests = b.addTest(.{ .name = "zig-worktree", .root_source_file = std.build.FileSource.relative("src/main.zig"), .target = target, .optimize = optimize });
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
@@ -41,22 +37,18 @@ pub fn build(b: *std.Build) void {
 fn addPaths(exe: *std.build.CompileStep) void {
     switch (builtin.target.os.tag) {
         .windows => {
-            exe.addIncludePath("./deps/include");
-            exe.addLibraryPath(thisDir() ++ "/deps/lib");
+            exe.addIncludePath(std.build.LazyPath.relative("./deps/include"));
+            exe.addLibraryPath(std.build.LazyPath.relative("./deps/lib"));
         },
         .freebsd, .openbsd, .netbsd, .linux => {
             // use system paths
         },
         .macos => {
-            exe.addIncludePath("/opt/homebrew/include");
-            exe.addLibraryPath("/opt/homebrew/lib");
+            exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/include" });
+            exe.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/lib" });
         },
         else => {
             @compileError("Platform is not supported!");
         },
     }
-}
-
-inline fn thisDir() []const u8 {
-    return comptime std.fs.path.dirname(@src().file) orelse ".";
 }
